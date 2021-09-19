5044ec6e4e660d3e0bd073f544c6b5323848d703
Ramya Mahesh <ramyamahesh@Vikas-MBP-2016.attlocal.net>
2021-09-19 16:33:36 -0700

    kubernetes project

---
+++ helloWorld.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main(){
+	r := gin.Default()
+	r.GET("/run", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	r.Run(":8080")
+}
